def699a3f9cbca22eab280267f3bcee3aedb2d77
Samuel T <ssamt1142@gmail.com>
2019-04-04 20:53:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentence = []
+    sentence << self.split(".")
+    sentence << self.split("?")
+    sentence << self.split("!")
+    sentence.reject(&:empty?)
+    if sentence.count == nil.to_i
+      nil.to_i
+    else
+      return sentence.count
+    end
   end
 end
